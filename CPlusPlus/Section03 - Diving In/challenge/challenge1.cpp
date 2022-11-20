d8ec4ef3f9f97046d7fa8e9590befaf5039cd773
elektronomous <akbarfaza222@gmail.com>
2022-11-20 23:40:53 +0800

    SECTION03 - Dive in

---
+++ CPlusPlus/Section03 - Diving In/challenge/challenge1.cpp
@@ -0,0 +1,19 @@
+// Print your name 10x times.
+
+#include <iostream>
+
+int main(void) {
+
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+    std::cout << "Muhammad Faza Akbar" << std::endl;
+
+    return 0;
+}
---
+++ CPlusPlus/Section03 - Diving In/main/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/*
+    this is multi line comment
+*/ 
+
+// this one line comment.
+
+int main(void) {
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+}
