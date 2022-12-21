0f6ec4bc392bc94a7e0e21460ababfac2e3ffea6
elektronomous <akbarfaza222@gmail.com>
2022-12-21 23:19:30 +0800

    set up project for section07

---
+++ CPlusPlus/Section07 - Diving In/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+consteval int get_value() {
+    return 3;
+}
+
+int main(int argc, char **argv) {
+    std::cout << "Hello World in C++20!" << std::endl;
+
+    return 0;
+}
