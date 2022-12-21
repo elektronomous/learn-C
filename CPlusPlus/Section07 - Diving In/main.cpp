755fd52cfc40b3273b00657450cf07f436c26477
elektronomous <akbarfaza222@gmail.com>
2022-12-22 07:27:41 +0800

    Exercises Section 1.2 done

--- CPlusPlus/Section07 - Diving In/main.cpp
+++ CPlusPlus/Section07 - Diving In/main.cpp
@@ -7,5 +7,10 @@ consteval int get_value() {
 int main(int argc, char **argv) {
     std::cout << "Hello World in C++20!" << std::endl;
 
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+
+
     return 0;
 }
