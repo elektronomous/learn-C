9a34871ff0098bc7619769ada2a6648dede4e312
elektronomous <akbarfaza222@gmail.com>
2023-05-02 21:51:13 +0800

    advance exercise

--- CPlusPlus/Books/C++ Primer/Chapter4/src/debugging.cpp
+++ CPlusPlus/Books/C++ Primer/Chapter4/src/debugging.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <vector>
+
+using std::cout,
+      std::cin,
+      std::endl,
+      std::vector,
+      std::string;
 
 int main(void) {
     unsigned short int short_val = 32768;
@@ -6,6 +13,11 @@ int main(void) {
     std::cout << short_val << std::endl;
 
     std::cout << -30 / 3 * 21 % 4 << std::endl;
+
+    const char *cp = " "; // empty string
+
+    if (*cp)
+        cout << "True, it's empty string" << endl;
 
     return 0;
 }
