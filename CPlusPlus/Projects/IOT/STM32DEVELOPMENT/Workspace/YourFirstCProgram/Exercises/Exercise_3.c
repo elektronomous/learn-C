0c1f3522588110adbfc9cac3911eb4d0ff3294ca
elektronomous <akbarfaza222@gmail.com>
2023-07-01 23:26:43 +0800

    First Section DONE

---
+++ CPlusPlus/Projects/IOT/STM32DEVELOPMENT/Workspace/YourFirstCProgram/Exercises/Exercise_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("David says , \" Programming is fun !\"\n");
+    printf("**Conditions apply , \"Offers valid until tommorrow\"\n");
+    printf("C:\\My computer\\My folder\n");
+    printf("D:/My Documents/My file\n");
+    printf("\\\\\\\\Today is holiday\\\\\\\\\n");
+    printf("This is a triple quoted string \"\"\" This month has 30 days \"\"\"\n");
+
+    return EXIT_SUCCESS;
+}
