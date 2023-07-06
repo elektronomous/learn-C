35b4b6f187d413aab53589de147b68c36f1f0ab0
elektronomous <akbarfaza222@gmail.com>
2023-07-06 22:28:06 +0800

    Seperate Compilation

---
+++ CPlusPlus/Books/C++ Primer/Chapter6 - Functions/src/Seperate Compilation Files/factMain.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Chapter6.hpp"
+
+using std::cin,
+      std::cout,
+      std::endl;
+
+int main(void) {
+    int j = fact(5);
+
+    cout << "5! : " << j << endl;
+
+    return 0;
+}
