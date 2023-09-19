7d1d86189a14de2e5cf245edecd93ba9b80a5079
elektronomous <akbarfaza222@gmail.com>
2023-09-20 07:29:07 +0800

    Chapter 10 BEGINNING

---
+++ CPlusPlus/Books/C++ Primer/Chapter10 - Generic Algorithm/src/usingFIND.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <array>
+
+using namespace std;
+
+int main(void) {
+    vector<int> vint {1,2,3,4,5};
+    array<int, 5> arrInt {1,2,3,4,5};
+    string str = "a value";
+
+    
+}
