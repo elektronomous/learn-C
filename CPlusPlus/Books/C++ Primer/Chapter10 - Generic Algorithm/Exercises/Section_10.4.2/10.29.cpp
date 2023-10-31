ef5a26533ccce16b0747ebc03a5e56e91c3e77a7
elektronomous <akbarfaza222@gmail.com>
2023-10-31 17:56:08 +0800

    EXERCISE 10.4.2 FIRST EXERCISE DONE

---
+++ CPlusPlus/Books/C++ Primer/Chapter10 - Generic Algorithm/Exercises/Section_10.4.2/10.29.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iterator>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        cout << argv[0] << " <input file>" << endl;
+    }
+    return 0;
+}
