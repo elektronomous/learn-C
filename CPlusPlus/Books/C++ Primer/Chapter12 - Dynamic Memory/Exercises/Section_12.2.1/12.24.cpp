b38c5988904286208bac9fc2c9f170f0d758ff8b
elektronomous <akbarfaza222@gmail.com>
2023-12-10 14:45:31 +0800

    EXERCISE SECTION 12.2.1 DONE

--- CPlusPlus/Books/C++ Primer/Chapter12 - Dynamic Memory/Exercises/Section_12.2.1/12.24.cpp
+++ CPlusPlus/Books/C++ Primer/Chapter12 - Dynamic Memory/Exercises/Section_12.2.1/12.24.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <cstring>
 
 using namespace std;
 
@@ -13,11 +14,24 @@ int main(void) {
         if (inputSz) {
             // allocate the char of this size
             char *allocChar = new char[inputSz];
-            // copy the input to the allocation char
-            strncpy(allocChar, inputStr.c_str(), inputStr.size());
 
-            // then show the result
-            cout << "After allocating, we show the result: " << allocChar << endl;
+            if (allocChar != nullptr){
+                // copy the input to the allocation char
+                strncpy(allocChar, inputStr.c_str(), inputStr.size());
+                // then show the result
+                cout << "After allocating, we show the result: " << allocChar << endl;
+
+                // delete the memory
+                delete[] allocChar;
+                allocChar = nullptr;
+            }
         }
     }
+
+    return 0;
 }
+
+/* This program handles the varying input size by get the size of the input first,
+   then store the size into inputSz. This inputSz will be the size of the allocated
+   array later.
+*/
